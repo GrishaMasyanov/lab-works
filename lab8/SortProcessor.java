9b5bef6536e3fcb74332becab15052400e0066c2
GrishaMasyanov <masyanov.grigoriy@bk.ru>
2026-05-30 18:47:11 +0300

    Create SortProcessor.java

---
+++ lab8/SortProcessor.java
@@ -0,0 +1,15 @@
+package eight;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SortProcessor {
+
+    @DataProcessor
+    public List<String> sort(List<String> data) {
+
+        return data.stream()
+                .sorted()
+                .collect(Collectors.toList());
+    }
+}
