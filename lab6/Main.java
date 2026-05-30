2480a37f0153fa2d13f70a7a0341dc89b544d021
GrishaMasyanov <masyanov.grigoriy@bk.ru>
2026-05-30 14:49:08 +0300

    Update Main.java

--- lab6/Main.java
+++ lab6/Main.java
@@ -1,1 +1,18 @@
+package six;
+public class Main {
+    public static void main(String[] args) {
+        SalesManager manager = new SalesManager();
 
+        manager.addSale(new Product("Milk", 80.0));
+        manager.addSale(new Product("Bread", 45.0));
+        manager.addSale(new Product("Milk", 80.0));
+        manager.addSale(new Product("Coffee", 350.0));
+        manager.addSale(new Product("Bread", 45.0));
+        manager.addSale(new Product("Milk", 80.0));
+
+        manager.printAllSales();
+        System.out.println("\n----------------------------");
+        System.out.println("Total income: " + manager.calculateTotalSum() + " rub.");
+        System.out.println("Most populat item: " + manager.getMostPopularProduct());
+    }
+}
